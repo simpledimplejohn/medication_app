29a43e4e07400b7f9032ec45c88287b64867197b
John Blalock <simpledimplejohn@gmail.com>
2024-05-26 18:13:44 -0700

    working

--- main_project/urls.py
+++ main_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('medication/', include('medication_app.urls')),
+    path('tomato/', include('app.urls')),
+    path('', include('medication_app.urls')),
 ]
